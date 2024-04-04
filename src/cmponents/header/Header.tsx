d0bb9746384f437e00dd8be05f8f34ccb5019624
Sergey <skvorcovsa90@gmail.com>
2024-04-05 02:48:13 +1000

    add header template

--- src/cmponents/header/Header.tsx
+++ src/cmponents/header/Header.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { MenuOutlined } from '@mui/icons-material'
+import {
+	AppBar,
+	Container,
+	Drawer,
+	IconButton,
+	Toolbar,
+	Typography,
+} from '@mui/material'
+import { useState } from 'react'
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
+
+	return (
+		<AppBar position='sticky' sx={{ mb: '10px' }}>
+			<Container maxWidth='xl'>
+				<Toolbar>
+					<Typography variant='h6' component='span' sx={{ flexGrow: '1' }}>
+						Material UI Experience
+					</Typography>
+					<IconButton color='inherit' onClick={() => setIsSideMenuOpen(true)}>
+						<MenuOutlined />
+					</IconButton>
+					<Drawer
+						anchor='right'
+						open={isSideMenuOpen}
+						onClose={() => setIsSideMenuOpen(false)}
+            // PaperProps={{
+            //   sx: { width: "10rem" },
+            // }}
+					>123</Drawer>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	)
 }
