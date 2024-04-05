491859e99713555acedb5f35d1ca42bb4f1149e6
Sergey <skvorcovsa90@gmail.com>
2024-04-06 03:12:40 +1000

    add new sliders to home page

--- src/modules/carousel/Carousel.tsx
+++ src/modules/carousel/Carousel.tsx
@@ -1,12 +1,21 @@
 import { Box } from '@mui/material'
 import Slide1 from '@/assets/images/carousel/slide_1.jpg'
 import Slide2 from '@/assets/images/carousel/slide_2.jpg'
+import Slide3 from '@/assets/images/carousel/slide_3.jpg'
+import Slide4 from '@/assets/images/carousel/slide_4.jpg'
+import Slide5 from '@/assets/images/carousel/slide_5.jpg'
+import Slide6 from '@/assets/images/carousel/slide_6.jpg'
+import Slide7 from '@/assets/images/carousel/slide_7.jpg'
+import Slide8 from '@/assets/images/carousel/slide_8.jpg'
 import { useEffect, useMemo, useState } from 'react'
-import { APP__SETTINGS } from '@/AppSetings'
+import { APP__SETTINGS } from '@/AppSettings'
 
 export const Carousel = () => {
-  const {homePageCarouselDelay} = APP__SETTINGS
-	const images = useMemo(() => [Slide1, Slide2], [])
+	const { homePageCarouselDelay } = APP__SETTINGS
+	const images = useMemo(
+		() => [Slide1, Slide2, Slide3, Slide4, Slide5, Slide6, Slide7, Slide8],
+		[]
+	)
 	const [carouselImage, setCarouselImage] = useState(Slide1)
 	const [currentIndex, setCurrentIndex] = useState(0)
 
