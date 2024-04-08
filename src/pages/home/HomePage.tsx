26f1f27e2dcebcdc771c0deb39a8489cbf3666ab
Sergey <skvorcovsa90@gmail.com>
2024-04-08 19:30:56 +1000

    add grid containers to home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,25 @@
+import { Container, Grid, Typography } from '@mui/material'
 import { Carousel } from '../../modules/carousel/Carousel'
 
 export const HomePage = () => {
 	return (
-		<>
-			<Carousel />
-		</>
+		<Container maxWidth='xl'>
+			<Grid container alignItems={'center'}>
+				<Grid item xs={6}>
+					<Carousel />
+				</Grid>
+				<Grid item xs={6}>
+					<Typography variant='h3' component='h3' align='center'>
+						Material UI is awesome!!!
+					</Typography>
+				</Grid>
+			</Grid>
+			{/* <Typography variant="h4" component="h4" align='left'>
+					Material UI is an open-source React component library
+					</Typography>
+					<Typography variant="h5" component="h5" align='right'>
+					Удобное, а главное, быстрое решение для создания UI компонентов
+					</Typography> */}
+		</Container>
 	)
 }
