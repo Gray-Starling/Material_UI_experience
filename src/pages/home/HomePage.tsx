8bf5fc5cde704c97b2a10acc7779a9b00946f4f3
Sergey <skvorcovsa90@gmail.com>
2024-04-08 20:01:02 +1000

    add text review component

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,11 @@
 import { Container, Grid, Typography } from '@mui/material'
 import { Carousel } from '../../modules/carousel/Carousel'
+import { TextReview } from '@/components/textReview/TextReview'
 
 export const HomePage = () => {
 	return (
 		<Container maxWidth='xl'>
-			<Grid container alignItems={'center'}>
+			<Grid container alignItems={'center'} sx={{ mb: '20px' }}>
 				<Grid item xs={6}>
 					<Carousel />
 				</Grid>
@@ -14,12 +15,7 @@ export const HomePage = () => {
 					</Typography>
 				</Grid>
 			</Grid>
-			{/* <Typography variant="h4" component="h4" align='left'>
-					Material UI is an open-source React component library
-					</Typography>
-					<Typography variant="h5" component="h5" align='right'>
-					Удобное, а главное, быстрое решение для создания UI компонентов
-					</Typography> */}
+			<TextReview />
 		</Container>
 	)
 }
